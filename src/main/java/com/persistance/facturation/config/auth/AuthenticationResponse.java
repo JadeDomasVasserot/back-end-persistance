a6d1b95ccf20d7de3470e36b3481bc46e329be1a
JadeDomasVasserot <jadedomasvasserot@gmail.com>
2023-03-01 09:20:39 +0100

    init back-end

---
+++ src/main/java/com/persistance/facturation/config/auth/AuthenticationResponse.java
@@ -0,0 +1,17 @@
+package com.mspr.arosaje.config.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+
+    private String token;
+    private int idUser;
+    private int role;
+}
---
+++ src/main/java/com/persistance/facturation/config/config/SecurityConfiguration.java
@@ -0,0 +1,64 @@
+package com.mspr.arosaje.config.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Profile("prod")
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                .and()
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/auth/**", "/swagger-ui/**", "/swagger/**", "/api-docs/**", "/error")
+                .permitAll()
+                .anyRequest()
+                .hasAnyAuthority(
+                        "Utilisateur",
+                        "Botaniste",
+                        "Administrateur",
+                        "Modérateur"
+                )                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+    @Profile("dev")
+    @Bean
+    public SecurityFilterChain securityFilterChainDev(HttpSecurity http) throws Exception {
+        http
+                .cors()
+                .and()
+                .csrf().disable()
+                .authorizeHttpRequests()
+                .anyRequest()
+                .permitAll();
+        return http.build();
+
+    }
+}
