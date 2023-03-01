1c503cf2d80b0a8421acfd875403aaf0ff169bc0
Erine <Berard.erine@gmail.com>
2023-03-01 14:27:40 +0100

    Ajout controller article et contient

--- src/main/java/com/persistance/facturation/services/ArticleService.java
+++ src/main/java/com/persistance/facturation/services/ArticleService.java
@@ -1,6 +1,7 @@
 package com.persistance.facturation.services;
 
 import com.persistance.facturation.models.Article;
+import com.persistance.facturation.models.Role;
 import com.persistance.facturation.repositories.ArticleRepository;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +22,20 @@ public class ArticleService {
         return this.articleRepository.findById(id);
     }
 
-    // TODO: 01/03/2023 Create 
+    public Article addArticle (Article article){
+        return this.articleRepository.save(article);
+    }
+
+    public Article updateArticle (Article article){
+        return this.articleRepository.save(article);
+    }
+
+    public void deleteArticle (int id){
+        this.articleRepository.deleteById(id);
+    }
+
+    public void deleteAllArticle (){
+        this.articleRepository.deleteAll();
+    }
 
 }
